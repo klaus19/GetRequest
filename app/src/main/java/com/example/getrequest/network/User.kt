55e434c2d4a694d140194c2901053d44d6b68e3c
klaus19 <tejaskhartude@gmail.com>
2022-12-20 08:45:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/getrequest/network/User.kt
@@ -0,0 +1,6 @@
+package com.example.getrequest.network
+
+data class User(val body: String,
+                val id: Int,
+                val title: String,
+                val userId: Int)
---
+++ app/src/main/java/com/example/getrequest/network/UserAPI.kt
@@ -0,0 +1,12 @@
+package com.example.getrequest.network
+
+import retrofit2.http.GET
+
+interface UserAPI {
+
+    @GET("posts/1")
+    suspend fun getPost(): User
+
+    @GET("posts")
+    suspend fun getPosts(): List<User>
+}
