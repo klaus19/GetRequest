4767fd7ccb4154e995243bb09428605571600b33
klaus19 <tejaskhartude@gmail.com>
2022-12-20 09:02:15 +0530

    updated

--- app/src/main/java/com/example/getrequest/MainActivity.kt
+++ app/src/main/java/com/example/getrequest/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.getrequest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.getrequest.databinding.ActivityMainBinding
+import com.example.getrequest.viewmodel.MainActivityViewmodel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: MainActivityViewmodel = ViewModelProvider(this)[MainActivityViewmodel::class.java]
+
+
+        viewModel.getPost()
+        viewModel.myResponse.observe(this, Observer {
+            Log.d(TAG, it.body)
+            Log.d(TAG, it.title)
+            Log.d(TAG, it.id.toString())
+            Log.d(TAG, it.userId.toString())
+        })
+
+        viewModel.getPosts()
+        viewModel.myResponseList.observe(this, Observer {
+            for (user in it) {
+                Log.d(TAG, user.body)
+                Log.d(TAG, user.title)
+                Log.d(TAG, user.id.toString())
+                Log.d(TAG, user.userId.toString())
+            }
+        })
     }
 }
